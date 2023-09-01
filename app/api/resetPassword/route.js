68b7228843a2968be29a91ea7ecb5c415b12f043
Shanti-PL <fanjunyi1998@163.com>
2023-09-01 23:21:25 +1000

    fix REST in resetPassword from POST to PUT

--- app/api/resetPassword/route.js
+++ app/api/resetPassword/route.js
@@ -3,7 +3,7 @@ import User from "@/models/user";
 import { NextResponse } from "next/server";
 import bcrypt from "bcryptjs";
 
-export async function POST(req) {
+export async function PUT(req) {
   try {
     const { email, password } = await req.json();
 
